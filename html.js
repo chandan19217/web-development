0ed1cb8f154f7703988e270f8bfb68dc6e2b6179
chandan19217 <19217@iiitu.ac.in>
2022-02-12 23:41:51 +0530

    All files commited for the first time

---
+++ html.js
@@ -0,0 +1,4 @@
+function ram()
+{
+  alert("iam chandan");
+}
---
+++ onclick.js
@@ -0,0 +1,10 @@
+function ram()
+{
+
+  document.getElementById("boring").innerHTML="new text!";
+  document.getElementById("boring").style.backgroundColor="red";
+  document.getElementById("boring").style.border="3px solid green";
+  document.body.style.backgroundColor="pink";
+
+
+}
